30fac9cb170e655da18758ab9a41ef0b25f2dd6a
caduuxx <cbcadulebe@gmail.com>
2024-09-08 15:46:35 -0300

    ConectFrontBack

---
+++ src/main/java/com/pagafacil/PagaFacil/WebConfig.java
@@ -0,0 +1,18 @@
+package com.pagafacil.PagaFacil;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // ou o URL do seu front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
